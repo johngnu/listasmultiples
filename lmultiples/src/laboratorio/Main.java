e73e784f66503dbcd7f610bfbdd4256900acd3dc
John Castillo <john.gnu@gmail.com>
2022-10-14 13:54:42 -0400

    ejer5

--- lmultiples/src/laboratorio/Main.java
+++ lmultiples/src/laboratorio/Main.java
@@ -26,7 +26,21 @@ public class Main {
         //mayorprecio(lp);
         //ejercicio2(lc, 1);
         ejercicio3(lc, "juan");
-
+        
+        ejercicio5("px", 60, 1, lc);
+        
+        lc.mostrar();
+        
+    }
+    
+    public static void ejercicio5(String n, int p, int x, ListaSC a) {
+        NodoC r = a.getP();
+        while (r != null) {
+            if (r.getNro() == x) {
+                r.getB().adiFinal(n, p); // nuevo producto
+            }
+            r = r.getSig();
+        }
     }
 
     public static void ejercicio3(ListaSC a, String x) {

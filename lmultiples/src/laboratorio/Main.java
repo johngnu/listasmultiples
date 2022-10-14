131681b1d7ce123091555c0c4019f88022ffa99c
John Castillo <john.gnu@gmail.com>
2022-10-14 13:34:33 -0400

    mayor precio

--- lmultiples/src/laboratorio/Main.java
+++ lmultiples/src/laboratorio/Main.java
@@ -1,6 +1,4 @@
-
 package laboratorio;
-
 
 public class Main {
 
@@ -10,22 +8,43 @@ public class Main {
         lp.adiFinal("p1", 5);
         lp.adiFinal("p2", 10);
         lp.adiFinal("p3", 15);
-        
-        lp.mostrar();
-        
+        //lp.mostrar();
+
         // expositores
         ListaSE le = new ListaSE();
         le.adiFinal("juan");
         le.adiFinal("oscar");
         le.adiFinal("ana");
-        
-        le.mostrar();
-        
+        //le.mostrar();
+
         // carpas 
         ListaSC lc = new ListaSC();
         lc.adiFinal(1, "ubic 1", le, lp);
-        
+
         lc.mostrar();
+        
+        // mayor precio
+        mayorprecio(lp);
+        
     }
-    
+
+    public static void mayorprecio(ListaDP a) {
+        int may = 0;
+        NodoP r = a.getP();
+        System.out.println("producto de mayor precio: ");
+        while (r != null) {
+            if (r.getPrecio() > may) {
+                may = r.getPrecio();
+            }
+            r = r.getSig();
+        }
+        r = a.getP();
+        while (r != null) {
+            if (r.getPrecio() == may) {
+                System.out.println("\t" + r.getNombre());
+            }
+            r = r.getSig();
+        }
+    }
+
 }
